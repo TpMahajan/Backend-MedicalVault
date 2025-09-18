4fc87f9c18e60e7c8c4b4a4d21402794eaa1e0fd
Tushar Mahajan <mahajantushar9041@gmail.com>
2025-09-18 21:27:37 +0530

    21.16

--- backend/routes/document.js
+++ backend/routes/document.js
@@ -6,7 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 import axios from "axios";
 import { auth } from "../middleware/auth.js";
 import { Document } from "../models/File.js";
-import User from "../models/User.js";
+import { User } from "../models/User.js";
 
 const router = express.Router();
 
